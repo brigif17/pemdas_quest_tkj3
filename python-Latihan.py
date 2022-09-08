d6146672ab8410af7abad247f0d6ae9da4f302e0
wisang <112366829+brigif17@users.noreply.github.com>
2022-09-07 23:23:42 -0700

    Add files via upload

---
+++ python-Latihan.py
@@ -0,0 +1,39 @@
+# RUMUS PERSEGI SISI * SISI
+
+print("\n1.Luas persegi")
+sisi = int(input("masukan nilai sisi: "))
+luas_P = sisi * sisi
+print("luas persegi adalah", luas_P)
+
+# RUMUS jajargenjang A * T
+
+print("\n1.Luas jajargenjang")
+Alas = int(input("masukan nilai sisi: "))
+Tinggi = int(input("masukan nilai sisi: "))
+luas_j = Alas * Tinggi
+print("luas jajargenjang adalah", luas_j)
+
+# RUMUS segitiga A * T / 2
+
+print("\n1.Luas segitiga")
+Alas = int(input("masukan nilai sisi: "))
+Tinggi = int(input("masukan nilai sisi: "))
+luas_S = Alas * Tinggi / 2
+print("luas segitiga adalah", luas_S)
+
+# RUMUS lingkaran 22/7 * r * r
+
+print("\n1.Luas lingkaran")
+phi  = 22/7
+r1 = int(input("masukan nilai sisi: "))
+r2 = int(input("masukan nilai sisi: "))
+luas_l = 22/7 * r1 * r2
+print("luas lingkaran adalah", luas_l)
+
+# RUMUS persegi panjang p * l
+
+print("\n1.Luas persegi panjang")
+Alas = int(input("masukan nilai sisi: "))
+Tinggi = int(input("masukan nilai sisi: "))
+luas_pp = Alas * Tinggi
+print("luas persegi panjang adalah", luas_pp)
