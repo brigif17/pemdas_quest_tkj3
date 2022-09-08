d85c345d27cc07ab2e39b2a52b9916b569975f6c
wisang <112366829+brigif17@users.noreply.github.com>
2022-09-07 23:24:45 -0700

    Add files via upload

---
+++ python8-tkj3.py
@@ -0,0 +1,23 @@
+list_kosong = []
+list_string = ["Juan", "Rasyad", "Wisanggeni", "Yoke"]
+#                 0       1          2            3  
+list_integer = [30, 27, 10, 13]
+list_mix = ["Juan", 30, True, 170]
+list_string2 = ["Juli", "September", "Maret", "September"]
+list_integer2 = [2007, 2006, 2006, 2006]
+
+print("\n1. List Nama dan Tanggal Lahir")
+print("Nama berikut:", list_string[0], "Tanggal lahir:", list_integer[0])
+print("Nama berikut:", list_string[1], "Tanggal lahir:", list_integer[1])
+print("Nama berikut:", list_string[2], "Tanggal lahir:", list_integer[2])
+print("Nama berikut:", list_string[3], "Tanggal lahir:", list_integer[3])
+
+print("\n2. List Nama, Tanggal dan Bulan Lahir")
+print("Nama berikut:", list_string[2]) 
+print("Tanggal,Bulan, dan Tahun lahir:", list_integer[2], list_string2[2], list_integer2[2])
+print("Nama berikut:", list_string[1]) 
+print("Tanggal,Bulan, dan Tahun lahir:", list_integer[1], list_string2[1], list_integer2[1])
+print("Nama berikut:", list_string[0]) 
+print("Tanggal,Bulan, dan Tahun lahir:", list_integer[0], list_string2[0], list_integer2[0])
+print("Nama berikut:", list_string[3]) 
+print("Tanggal,Bulan, dan Tahun lahir:", list_integer[3], list_string2[3], list_integer2[3])
