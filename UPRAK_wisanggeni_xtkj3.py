8aced3ff024a543e80b3f73a5f8995317f0b2047
wisang <112366829+brigif17@users.noreply.github.com>
2022-09-08 19:36:18 -0700

    Add files via upload

---
+++ UPRAK_wisanggeni_xtkj3.py
@@ -0,0 +1,45 @@
+# MENGHITUNG VOLUME BANGUNAN RUANG
+
+
+# MENGHITUNG VOLUME BALOK P * L * T
+
+print("\n1.volume balok")
+panjang = int(input("masukan nilai panjang: "))
+lebar = int(input("masukan nilai lebar: "))
+tinggi = int(input("masukan nilai tinggi: "))
+volume_b = panjang * lebar * tinggi
+print("volume balok adalah", volume_b)
+
+# MENGHITUNG VOLUME KUBUS S * S * S
+
+print("\n2.volume kubus")
+sisi = int(input("masukan nilai sisi: "))
+sisi = int(input("masukan nilai sisi: "))
+sisi = int(input("masukan nilai sisi: "))
+volume_k = sisi * sisi * sisi
+print("volume kubus adalah", volume_k)
+
+# MENGHITUNG VOLUME LIMAS 1/3 * LA * T
+
+print("\n3.volume limas")
+la = int(input("masukan nilai luas alas: "))
+t = int(input("masukan nilai tinggi: "))
+volume_l = 1/3 * la * t
+print("volume limas adalah", volume_l)
+
+# MENGHITUNG VOLUME TABUNG 22/7 * R * R * T
+
+print("\n4.volume tabung")
+r1 = int(input("masukan nilai r1: "))
+r2 = int(input("masukan nilai r2: "))
+t = int(input("masukan nilai t: "))
+volume_t = 22/7 * r1 * r2 * t
+print("volume kubus adalah", volume_t)
+
+# Celcius to Reamur
+
+print("\n1. Celcius to Reamur")
+Celcius = int(input("masukan nilai Celcius: "))
+Reamur = 4/5 * Celcius
+print("Reamur adalah", Reamur)
+
